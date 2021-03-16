df1be0f0f8a7c1801579586594668f0f9446f4fc
Vagner da Silva Barbosa <vsbarbosa@server01.foursys.local>
2021-03-16 17:26:48 -0300

    Install GP e Hooks Form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {StyledComponent} from 'styled-components'
+import UseForm from './components/UseForm'
 
 function App() {
   return (
+    <>
+    <UseForm />
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Vagner</h1>
     </div>
+
+    </>
   );
 }
 
---
+++ src/stories/Header.stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Header } from './Header';
+
+const user =  {
+  title: 'Example/Header',
+  component: Header,
+};
+
+const Template = (args) => <Header {...args} />;
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  user: {},
+};
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {};
+
+export default user
